ec707c1f2cc52668688f6b7732fc0c9b47721cd4
unknown <chohankoti333@gmail.com>
2024-05-30 20:13:21 +0530

    Pagebtn code completed

--- src/components/Pagebtn.jsx
+++ src/components/Pagebtn.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import Contextpage from '../Contextpage';
+import Button from '../assets/Btn'
+import { HiChevronUp } from "react-icons/hi";
 
-export default function Pagebtn() {
-  return (
-    <div>Pagebtn</div>
-  )
+export const Pagebtn = () => {
+
+    const { setPage, page } = useContext(Contextpage);
+
+    return (
+        <>
+            <div className='btnpanel flex justify-center items-center'>
+                <a href='#' onClick={() => setPage(page - 1)}><Button item="Back" /></a>
+                <div className='px-4 py-2 bg-slate-700  text-white font-semibold rounded-full'>{page}</div>
+                <a href='#' onClick={() => setPage(page + 1)}><Button item="Next" /></a>
+            </div>
+
+        </>
+    )
 }
